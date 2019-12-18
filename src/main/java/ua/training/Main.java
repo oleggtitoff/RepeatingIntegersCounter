638dee0fc28fefb0070e6c63b4904dffb56a94fe
Oleg Titov <oleggtitoff@gmail.com>
2019-12-18 22:25:10 +0200

    Added array and stream to count integers.

--- src/main/java/ua/training/Main.java
+++ src/main/java/ua/training/Main.java
@@ -1,9 +1,21 @@
 package ua.training;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<Integer> arrayList
+                = new ArrayList<>(Arrays.asList(4, 5, -6, 4, 5, 3, 4, 2, 4, 5, 7));
 
+        arrayList.stream()
+                .collect(Collectors.groupingBy(
+                        Integer::intValue,
+                        () -> new TreeMap<>(Comparator.comparingInt(Integer::intValue)),
+                        Collectors.counting()
+                ))
+                .forEach((k, v) -> System.out.println(k + " - " + v));
     }
 
 }
